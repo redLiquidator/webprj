5c5ddea4704c09a1cb77497f34d5b3e773bc609d
redLiquidator <inufuni@naver.com>
2020-12-31 16:10:50 +0900

    project setting index.jsp dispatcher-servlet.xml IndexController.java

---
+++ src/main/java/com/newlectures/webprj/App.java
@@ -0,0 +1,13 @@
+package com.newlectures.webprj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
